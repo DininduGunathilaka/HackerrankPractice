e9ed0425822dc93f6907d5100c4be9e841d8487f
DiniX99 <73194227+DininduGunathialaka@users.noreply.github.com>
2022-04-12 09:03:36 +0530

    Create Day 3_Intro to Conditional Statements.java

---
+++ 30 Days od Code/Day 3_Intro to Conditional Statements.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+        if((N % 2 != 0)||((N>5)&&(N<21))){
+            System.out.println("Weird");
+        }
+        else
+        System.out.println("Not Weird");
+    }
+}
