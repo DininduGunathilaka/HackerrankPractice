89ba7c1b31280c103685b1d1fa48d62a785a7cb1
DiniX99 <73194227+DininduGunathilaka@users.noreply.github.com>
2022-04-16 10:38:42 +0530

    Create Sum of Digits of a Five Digit Number.c

---
+++ C/Sum of Digits of a Five Digit Number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n,sum=0,remainder;
+    scanf("%d", &n);
+    while(n>0)
+    {
+       remainder=n%10;
+       sum=sum+remainder;
+       n=n/10;
+        
+    }
+    printf("%d",sum);
+    return 0;
+}
